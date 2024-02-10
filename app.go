e8b46dbda0ae34fda89aec8918c3330c47e4566c
Isaac <inunez.dev@gmail.com>
2024-02-10 19:17:17 +0700

    Created board and rendered it to the console

--- app.go
+++ app.go
@@ -4,57 +4,177 @@ import (
 	"fmt"
 )
 
+const WHITE string = "w"
+const BLACK string = "b"
+
 type piece struct {
+	color    string
 	position string
 	name     string
 }
 
 type position struct {
-	piece piece
+	piece *piece
 	name  string
 }
 
 type board struct {
-	spaces [][]int
+	spaces [][]*position
 }
 
-func newBoard(w int, h int) *board {
-	s := make([][]int, w)
-	for i := range s {
-		s[i] = make([]int, h)
+func newBoard() *board {
+	s := [][]*position{
+		{
+			newPosition(newRook(BLACK)),
+			newPosition(newKnight(BLACK)),
+			newPosition(newBishop(BLACK)),
+			newPosition(newQueen(BLACK)),
+			newPosition(newKing(BLACK)),
+			newPosition(newBishop(BLACK)),
+			newPosition(newKnight(BLACK)),
+			newPosition(newRook(BLACK)),
+		},
+		{
+			newPosition(newPawn(BLACK)),
+			newPosition(newPawn(BLACK)),
+			newPosition(newPawn(BLACK)),
+			newPosition(newPawn(BLACK)),
+			newPosition(newPawn(BLACK)),
+			newPosition(newPawn(BLACK)),
+			newPosition(newPawn(BLACK)),
+			newPosition(newPawn(BLACK)),
+		},
+		{
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+		},
+		{
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+		},
+		{
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+		},
+		{
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+			newEmptyPosition(),
+		},
+		{
+			newPosition(newPawn(WHITE)),
+			newPosition(newPawn(WHITE)),
+			newPosition(newPawn(WHITE)),
+			newPosition(newPawn(WHITE)),
+			newPosition(newPawn(WHITE)),
+			newPosition(newPawn(WHITE)),
+			newPosition(newPawn(WHITE)),
+			newPosition(newPawn(WHITE)),
+		},
+		{
+			newPosition(newRook(WHITE)),
+			newPosition(newKnight(WHITE)),
+			newPosition(newBishop(WHITE)),
+			newPosition(newQueen(WHITE)),
+			newPosition(newKing(WHITE)),
+			newPosition(newBishop(WHITE)),
+			newPosition(newKnight(WHITE)),
+			newPosition(newRook(WHITE)),
+		},
 	}
 	board := board{spaces: s}
 	return &board
 }
 
+func newPosition(piece *piece) *position {
+	return &position{piece: piece}
+}
+
+func newEmptyPosition() *position {
+	return newPosition(newNoPiece())
+}
+
+func newPiece(name string, color string) *piece {
+	return &piece{name: name, color: color}
+}
+
+func newNoPiece() *piece {
+	return &piece{name: "", color: ""}
+}
+
+func newPawn(color string) *piece {
+	return newPiece("pawn", color)
+}
+
+func newRook(color string) *piece {
+	return newPiece("rook", color)
+}
+
+func newBishop(color string) *piece {
+	return newPiece("bishop", color)
+}
+
+func newKing(color string) *piece {
+	return newPiece("king", color)
+}
+
+func newQueen(color string) *piece {
+	return newPiece("queen", color)
+}
+
+func newKnight(color string) *piece {
+	return newPiece("knight", color)
+}
+
+func renderBoard(board *board) {
+	var p *piece
+	fmt.Print("     a    b    c    d    e    f    g    h  \n")
+	fmt.Print("   ----------------------------------------\n")
+	row := 8
+	for i := range board.spaces {
+		fmt.Printf("%d |", row)
+		for j := range board.spaces[i] {
+			p = board.spaces[i][j].piece
+			if p.name != "" {
+				fmt.Printf(" %c", p.name[0])
+				fmt.Print(p.color)
+				fmt.Print(" ")
+			} else {
+				fmt.Print("  * ")
+			}
+			fmt.Print(" ")
+		}
+		fmt.Printf("| %d \n", row)
+		row--
+	}
+	fmt.Print("   ----------------------------------------\n")
+	fmt.Print("     a    b    c    d    e    f    g    h  \n")
+}
+
 func main() {
-
-	fmt.Println(newBoard(3, 5))
-	// fmt.Println(person{"Bob", 20})
-
-	// fmt.Println(person{name: "Alice", age: 30})
-
-	// fmt.Println(person{name: "Fred"})
-
-	// fmt.Println(&person{name: "Ann", age: 40})
-
-	// fmt.Println(newPerson("Jon"))
-
-	// s := person{name: "Sean", age: 50}
-	// fmt.Println(s.name)
-
-	// sp := &s
-	// fmt.Println(sp.age)
-
-	// sp.age = 51
-	// fmt.Println(sp.age)
-
-	// dog := struct {
-	//     name   string
-	//     isGood bool
-	// }{
-	//     "Rex",
-	//     true,
-	// }
-	// fmt.Println(dog)
+	myBoard := newBoard()
+	renderBoard(myBoard)
 }
